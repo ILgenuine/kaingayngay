3ac57b260d35506434ccc9b1732abb8d2aad6dc6
ilgenuine <infx@live.se>
2018-04-17 14:32:25 +0700

    update

---
+++ allproduct.php
@@ -0,0 +1,255 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <?php include_once('inc/header.php'); ?>
+  </head>
+
+  <body style="background-color:#EEE;">
+
+    <?php include_once('inc/navbar.php'); ?>
+
+<div class="container nav-container">
+
+      <div class="row row-content mt-4 mb-5 mx-0">
+
+
+        <div class="col-lg-3 col-10 px-0 mx-auto ">
+
+          <div class="mb-3 ml-lg-0 ml-md-0 ml-3">
+            <a class="text-black" href="index.php">หน้าหลัก</a> > <label class="text-blue">รวมประกาศ</label>
+          </div>
+
+
+          <div class="mb-3 ml-lg-0 ml-md-0 ml-3 editprofile-btn">
+            <h3 class="text-pink mb-0">
+              รวมประกาศ
+            </h3>
+          </div>
+
+          <div class="mb-3 ml-lg-0 ml-md-0 ml-3">
+                <label class="form-check-label text-dark" style="font-weight: 600;">
+                     เลือกช่วงราคา
+                </label>
+          </div>
+
+
+          <div class="mb-lg-3 mb-md-3 mb-3 ml-lg-0 ml-md-0 ml-md-0">
+            <div class="row ml-lg-0 ml-md-0 ml-3">
+                <div class="col-5 pl-0">
+                  <input type="text" id="" class="form-control mx-lg-0 mx-md-0 mx-sm-3" placeholder="50,000">
+                </div>
+                <div class="col-1 pl-lg-0 pl-md-2 pl-0 ">
+                    <label class="mb-0 mt-2">ถึง</label>
+                </div>
+                <div class="col-5 pl-0">
+                  <input type="text" id="" class="form-control mx-lg-0  mx-md-0 mx-sm-3" placeholder="500,000">
+                </div>
+            </div>
+          </div>
+
+          <div class="mb-lg-5 mb-md-5 mb-4 ml-lg-0 ml-md-0 ml-md-0">
+            <button class="mx-lg-0 mx-3 nav-link login-btn">
+                <p class=" mb-0">
+                  <i class="fa fa-search fa-lg"></i>
+                  ค้นหา
+                </p>
+            </button>
+          </div>
+
+
+
+          <!-- แอดจอใหญ่  (มีแอดจอเล็กข้างล่างด้วย) -->
+          <div class="d-lg-block d-md-none d-none mb-3 ml-lg-0 ml-md-0 px-0">
+            <a href="#">
+              <img class="" src="assets/images/sponsor/270x500-demo.jpg" alt="" title="" width="90%" height="100%">
+            </a>
+          </div>
+
+        </div>
+
+        <div class="mx-md-auto col-lg-9 col-md-11 col-12 px-0">
+
+
+              <div class="row mt-lg-5">
+
+                <div class="col-lg-4 col-md-6 col-12 px-lg-3 px-md-3 px-5 mb-lg-3 mb-4">
+                  <a href="productdetail.php">
+                        <div class="img-frame">
+                            <img class="card-img-top" src="assets/images/car/car1.jpg" alt="Card image cap">
+                        </div>
+                  </a>
+                        <div class="card-body px-0 pt-3">
+                            <h5 class="card-title text-pink">รถบ้าน Toyota Camry เจ้าของขายเอง</h5>
+                            <p class="card-text text-dark">ปี 2003, กรุงเทพมหนคร</p>
+                        </div>
+                        <div class="pull-right">
+                          <label class="card-price text-blue">435,000</label>
+                          <label class="card-price text-dark">บาท</label>
+                        </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 col-12 px-lg-3 px-md-3 px-5 mb-lg-3 mb-4">
+                  <a href="productdetail.php">
+                        <div class="img-frame">
+                            <img class="card-img-top" src="assets/images/car/car2.jpg" alt="Card image cap">
+                        </div>
+                  </a>
+                        <div class="card-body px-0 pt-3">
+                            <h5 class="card-title text-pink">รถบ้าน Toyota Camry เจ้าของขายเอง</h5>
+                            <p class="card-text text-dark">ปี 2003, กรุงเทพมหนคร</p>
+                        </div>
+                        <div class="pull-right">
+                          <label class="card-price text-blue">435,000</label>
+                          <label class="card-price text-dark">บาท</label>
+                        </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 col-12 px-lg-3 px-md-3 px-5 mb-lg-3 mb-4">
+                  <a href="productdetail.php">
+                        <div class="img-frame">
+                            <img class="card-img-top" src="assets/images/car/car3.jpg" alt="Card image cap">
+                        </div>
+                  </a>
+                        <div class="card-body px-0 pt-3">
+                            <h5 class="card-title text-pink">รถบ้าน Toyota Camry เจ้าของขายเอง</h5>
+                            <p class="card-text text-dark">ปี 2003, กรุงเทพมหนคร</p>
+                        </div>
+                        <div class="pull-right">
+                          <label class="card-price text-blue">435,000</label>
+                          <label class="card-price text-dark">บาท</label>
+                        </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 col-12 px-lg-3 px-md-3 px-5 mb-lg-3 mb-4">
+                  <a href="productdetail.php">
+                        <div class="img-frame">
+                            <img class="card-img-top" src="assets/images/car/car1.jpg" alt="Card image cap">
+                        </div>
+                  </a>
+                        <div class="card-body px-0 pt-3">
+                            <h5 class="card-title text-pink">รถบ้าน Toyota Camry เจ้าของขายเอง</h5>
+                            <p class="card-text text-dark">ปี 2003, กรุงเทพมหนคร</p>
+                        </div>
+                        <div class="pull-right">
+                          <label class="card-price text-blue">435,000</label>
+                          <label class="card-price text-dark">บาท</label>
+                        </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 col-12 px-lg-3 px-md-3 px-5 mb-lg-3 mb-4">
+                  <a href="productdetail.php">
+                        <div class="img-frame">
+                            <img class="card-img-top" src="assets/images/car/car2.jpg" alt="Card image cap">
+                        </div>
+                  </a>
+                        <div class="card-body px-0 pt-3">
+                            <h5 class="card-title text-pink">รถบ้าน Toyota Camry เจ้าของขายเอง</h5>
+                            <p class="card-text text-dark">ปี 2003, กรุงเทพมหนคร</p>
+                        </div>
+                        <div class="pull-right">
+                          <label class="card-price text-blue">435,000</label>
+                          <label class="card-price text-dark">บาท</label>
+                        </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 col-12 px-lg-3 px-md-3 px-5 mb-lg-3 mb-4">
+                  <a href="productdetail.php">
+                        <div class="img-frame">
+                            <img class="card-img-top" src="assets/images/car/car3.jpg" alt="Card image cap">
+                        </div>
+                  </a>
+                        <div class="card-body px-0 pt-3">
+                            <h5 class="card-title text-pink">รถบ้าน Toyota Camry เจ้าของขายเอง</h5>
+                            <p class="card-text text-dark">ปี 2003, กรุงเทพมหนคร</p>
+                        </div>
+                        <div class="pull-right">
+                          <label class="card-price text-blue">435,000</label>
+                          <label class="card-price text-dark">บาท</label>
+                        </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 col-12 px-lg-3 px-md-3 px-5 mb-lg-3 mb-4">
+                  <a href="productdetail.php">
+                        <div class="img-frame">
+                            <img class="card-img-top" src="assets/images/car/car1.jpg" alt="Card image cap">
+                        </div>
+                  </a>
+                        <div class="card-body px-0 pt-3">
+                            <h5 class="card-title text-pink">รถบ้าน Toyota Camry เจ้าของขายเอง</h5>
+                            <p class="card-text text-dark">ปี 2003, กรุงเทพมหนคร</p>
+                        </div>
+                        <div class="pull-right">
+                          <label class="card-price text-blue">435,000</label>
+                          <label class="card-price text-dark">บาท</label>
+                        </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 col-12 px-lg-3 px-md-3 px-5 mb-lg-3 mb-4">
+                  <a href="productdetail.php">
+                        <div class="img-frame">
+                            <img class="card-img-top" src="assets/images/car/car2.jpg" alt="Card image cap">
+                        </div>
+                  </a>
+                        <div class="card-body px-0 pt-3">
+                            <h5 class="card-title text-pink">รถบ้าน Toyota Camry เจ้าของขายเอง</h5>
+                            <p class="card-text text-dark">ปี 2003, กรุงเทพมหนคร</p>
+                        </div>
+                        <div class="pull-right">
+                          <label class="card-price text-blue">435,000</label>
+                          <label class="card-price text-dark">บาท</label>
+                        </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 col-12 px-lg-3 px-md-3 px-5 mb-lg-3 mb-4">
+                  <a href="productdetail.php">
+                        <div class="img-frame">
+                            <img class="card-img-top" src="assets/images/car/car3.jpg" alt="Card image cap">
+                        </div>
+                  </a>
+                        <div class="card-body px-0 pt-3">
+                            <h5 class="card-title text-pink">รถบ้าน Toyota Camry เจ้าของขายเอง</h5>
+                            <p class="card-text text-dark">ปี 2003, กรุงเทพมหนคร</p>
+                        </div>
+                        <div class="pull-right">
+                          <label class="card-price text-blue">435,000</label>
+                          <label class="card-price text-dark">บาท</label>
+                        </div>
+                </div>
+
+                <div class="col-12 mx-auto px-auto">
+                      <ul class="pagination justify-content-center">
+                        <li class="page-item mx-1"><a class="page-link" href="#">Previous</a></li>
+                        <li class="page-item mx-1"><a class="page-link" href="#">1</a></li>
+                        <li class="page-item mx-1 active"><a class="page-link" href="#">2</a></li>
+                        <li class="page-item mx-1"><a class="page-link" href="#">3</a></li>
+                        <li class="page-item mx-1"><a class="page-link" href="#">Next</a></li>
+                      </ul>
+                </div>
+
+
+              </div>
+
+          </div>
+
+
+          <!-- แอดจอเล็ก  (มีแอดจอใหญ่ข้างบนด้วย) -->
+          <div class="d-lg-none d-md-block d-block col-lg-4 col-10 px-0 mx-auto align-center mt-5 das-overide-col">
+              <div>
+                <a href="#">
+                  <img src="assets/images/sponsor/270x500-demo.jpg" alt="" title="" width="270px" height="500px">
+                </a>
+              </div>
+          </div>
+
+    </div>
+
+</div>
+
+
+    <?php include_once('inc/footer.php'); ?>
+    <?php include_once('inc/footer-script.php'); ?>
+    </script>
+
+
+  </body>
+</html>
